b75fc276f6a7f993b0265757ecd0db621efee1fa
Sofia Pogosian <sonyapgsn@gmail.com>
2024-10-08 17:14:12 +0300

    test_commit

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='titles'
+    )
+    genres = models.ManyToManyField(
+        Genre, related_name='titles')
+
+    def __str__(self):
+        return self.name
