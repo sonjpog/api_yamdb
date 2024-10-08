c8d5e0b46161dca021b98cb3300b993b052f23d0
Leonid Zykov <leonidzykov999@gmail.com>
2024-10-08 21:38:38 +0300

    leo first commit

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, serializers as ser
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.db import IntegrityError
+from reviews.models import Review, Titles
+from .serializers import ReviewSerializer
 
-# Create your views here.
+
+class MockUser:
+    """Модель-заглушка для пользователя."""
+    def __init__(self, username='mockuser'):
+        self.username = username
+
+    @property
+    def is_authenticated(self):
+        return True
+
+
+class MockPermission:
+    """Заглушка для проверки прав доступа."""
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        return True
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (MockPermission,)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        return Review.objects.filter(title=title_id)
+
+    def get_title(self):
+        title_id = self.kwargs.get('title_id')
+        return get_object_or_404(Titles, pk=title_id)
+
+    def perform_create(self, serializer):
+        try:
+            # Используем MockUser вместо self.request.user
+            mock_user = MockUser()
+            serializer.save(author=mock_user, title=self.get_title())
+        except IntegrityError:
+            raise ser.ValidationError("Не удается создать повторяющийся отзыв.")
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance,
+            data=request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
