71280dfa21aaa15056041b873493cefdb1b10922
Sofia Pogosian <sonyapgsn@gmail.com>
2024-10-09 17:54:09 +0300

    first-commit

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+
+from reviews.models import Category, Genre, Title
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        exclude = ('id', )
+        model = Category
+        lookup_field = 'slug'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        exclude = ('id', )
+        model = Genre
+        lookup_field = 'slug'
+
+
+class TitleReadSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(
+        read_only=True,
+        many=True
+    )
+    rating = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Title
+
+
+class TitleСhangeSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug'
+    )
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        slug_field='slug',
+        many=True
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Title
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from reviews.models import Category, Genre, Title
+
+from .filters import TitleFilter
+from .mixins import BasicActionsViewSet
+from .serializers import (CategorySerializer,
+                          GenreSerializer,
+                          TitleСhangeSerializer,
+                          TitleReadSerializer)
+
+
+class TitlesViewSet(ModelViewSet):
+    """Получить список всех объектов."""
+    queryset = Title.objects.all()
+    serializer_class = TitleReadSerializer
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.request.method in ('PATCH', 'POST'):
+            return TitleСhangeSerializer
+        return TitleReadSerializer
+
+
+class CategoryViewSet(BasicActionsViewSet):
+    """Получить список всех категорий."""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = (SearchFilter, )
+    search_fields = ('name', )
+    lookup_field = 'slug'
+
+
+class GenreViewSet(BasicActionsViewSet):
+    """Получить список всех жанров."""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', )
+    lookup_field = 'slug'
