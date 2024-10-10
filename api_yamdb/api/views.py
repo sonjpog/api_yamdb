1e1b985db47cbb988e578eecc882f3556a1646e9
Sofia Pogosian <sonyapgsn@gmail.com>
2024-10-10 12:06:08 +0300

    small fixes

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -78,7 +78,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def get_title(self):
         title_id = self.kwargs.get('title_id')
-        return get_object_or_404(Titles, pk=title_id)
+        return get_object_or_404(Title, pk=title_id)
 
     def perform_create(self, serializer):
         try:
