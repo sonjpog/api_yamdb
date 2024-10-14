ef931c5efe362679ebb9fee1c5cec96bdcee80cc
Leonid Zykov <leonidzykov999@gmail.com>
2024-10-14 13:38:34 +0300

    fix review and comment

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth import get_user_model
-from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
-from rest_framework import serializers as ser, status, viewsets
+from rest_framework import status, viewsets
 from rest_framework import permissions
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, PermissionDenied
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -13,7 +12,7 @@ from api.serializers import UserSerializer, TokenSerializer
 from reviews.models import Category, Comment, Genre, Review, Title
 from .filters import TitleFilter
 from .mixins import BasicActionsViewSet
-from .permissions import IsAdmin, IsAuthorOrReadOnly
+from .permissions import IsAdmin, IsAuthorOrReadOnly, IsModerator
 from .serializers import (
     CategorySerializer,
     CommentSerializer,
@@ -137,7 +136,9 @@ class GenreViewSet(BasicActionsViewSet):
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+    # Любой может просматривать отзывы, но на другие действия нужны права
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         title_id = self.kwargs.get('title_id')
@@ -149,32 +150,31 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         title = self.get_title()
-        user_review_exists = Review.objects.filter(
-            title=title,
-            author=self.request.user
-        ).exists()
-        if user_review_exists:
-            raise ValidationError("На одно произведение пользователь",
-                                  "может оставить только один отзыв.")
+        if Review.objects.filter(title=title, author=self.request.user).exists():
+            raise ValidationError("На одно произведение пользователь может оставить только один отзыв.")
         serializer.save(author=self.request.user, title=title)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            self.perform_create(serializer)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, *args, **kwargs):
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def partial_update(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.author != request.user and not (request.user.is_staff or request.user.role in ['moderator', 'admin']):
+            raise PermissionDenied("У вас нет прав на удаление этого отзыва.")
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         review_id = self.kwargs.get('review_id')
@@ -183,3 +183,8 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
         serializer.save(author=self.request.user, review=review)
+
+    def update(self, request, *args, **kwargs):
+        if request.method == 'PUT':
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().update(request, *args, **kwargs)
